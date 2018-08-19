bdcc602d50e3e462e63c70e29cf51fa83f02feb2
Edin Jašarević <edinjasar14@gmail.com>
2018-08-19 17:06:19 +0200

    web servis

---
+++ WebService Calculator/CalculatorWebService.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService_Calculator
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://localhost:3591/CalculatorWebService/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        [WebMethod]
+        public int Sub(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        [WebMethod]
+        public int Div(int firstNumber, int secondNumber)
+        {
+            if (secondNumber == 0) return 0;
+            return firstNumber / secondNumber;
+        }
+
+        [WebMethod]
+        public int Mult(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
